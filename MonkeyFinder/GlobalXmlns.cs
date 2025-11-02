19be7d04eef6f9383cef5f227a40d97762d355c8
Dee Jae Mann <dee.j.mann@hotmail.com>
2025-11-02 14:54:06 -0500

    Added project template files

---
+++ MonkeyFinder/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MonkeyFinder")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MonkeyFinder.Pages")]
---
+++ MonkeyFinder/View/DetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MonkeyFinder;
+
+public partial class DetailsPage : ContentPage
+{
+    public DetailsPage()
+    {
+        InitializeComponent();
+    }
+}
